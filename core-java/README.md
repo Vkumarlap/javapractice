6cdb69ed7a688adeef29624c7dc67ee9dc5b9a7e
Vkumarlap <vangakumar8@gmail.com>
2026-01-17 22:20:16 +0530

    Implemented Java interfaces for loose coupling

---
+++ core-java/README.md
@@ -0,0 +1,10 @@
+# Java Daily Practice
+
+This repository contains my daily Java practice code,
+covering core Java, OOP concepts, collections, Java 8 features,
+and Spring basics.
+
+Purpose:
+- Strengthen Java fundamentals
+- Practice clean code
+- Track daily learning progress
---
+++ core-java/interfaceTutorial.java
@@ -0,0 +1,33 @@
+interface computer{
+     void code();
+}
+
+class laptop implements computer{
+   public void code(){
+        System.out.println("I'm laptop");
+    }
+}
+
+class desktop implements computer{
+   public void code(){
+        System.out.println("desktop");
+    }
+}
+
+class devloper{
+    void devapp(computer lap){
+            lap.code();
+    }
+}
+class interfaceTutorial{
+    public static void main(String a[])
+        {
+       computer lap=new laptop();
+       computer desk=new desktop();
+       devloper dev1=new devloper();
+
+          dev1.devapp(desk);
+          dev1.devapp(lap);
+
+        }
+}
