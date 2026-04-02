691a201b8966bfacfb7ed30880af983eb996016c
Vkumarlap <vangakumar8@gmail.com>
2026-04-03 00:25:08 +0530

    implementing Docking Exception Using Throws

---
+++ corejava/DockingExceptionUsingThrows.java
@@ -0,0 +1,34 @@
+class Demo {
+
+    void A() throws ClassNotFoundException {
+
+        Class.forName("calc");
+
+    }
+
+    void B() throws Exception {
+
+        A();
+    }
+
+    void C() {
+
+        try {
+            B();
+            A();
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+
+    }
+
+}
+
+public class DockingExceptionUsingThrows {
+    public static void main(String[] args)  {
+        
+        Demo obj=new Demo();
+        obj.C();
+        
+    }
+}
